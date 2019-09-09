2d0e92008b4d2d8affdbc8cff92aacb8caad134e
Optimus Prime <www.usamaalam60@gmail.com>
2019-09-10 01:45:26 +0500

    Contact us Page ... Second Page Search Page Settings

---
+++ KoreaOnly/Controllers/ContactusController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KoreaOnly.Controllers
+{
+    [RoutePrefix("SystemMaster/Contactus")]
+    public class ContactusController : Controller
+    {
+        [Route("")]
+        [Route("SystemMaster/Contactus/Index")]
+        // GET: Contactus
+        public ActionResult Index()
+        {
+
+            if (MainController.checkAdminLogin())
+            {
+                using (var DB = new DbConnection())
+                {
+                    var Re = DB.GetResult<KoreaOnly.Controllers.IndexController.CommentForm>("select  * From CommentForm order by CommentFormID desc");
+
+                    return View(Re);
+                }
+
+            }
+
+            return Redirect("/SystemMaster/");
+        }
+    }
+}
